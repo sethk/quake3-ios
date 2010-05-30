a7b2e38b33cdfeaf9713f0ab968db4de311e2346
sethk@meowfishies.com <sethk@meowfishies.com@b817fe96-c947-0410-a8fc-cf2ef4c6330a>
2010-05-30 03:25:14 +0000

    Fix for iPhone simulator.
    git-svn-id: https://quake3-iphone.googlecode.com/svn/trunk@77 b817fe96-c947-0410-a8fc-cf2ef4c6330a

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2823,7 +2823,7 @@ void Com_Shutdown (void) {
 
 #if !( defined __VECTORC )
 #if !( defined __linux__ || defined __FreeBSD__ )  // r010123 - include FreeBSD 
-#if ((!id386) && (!defined __i386__)) // rcg010212 - for PPC
+#if ((!id386) && (!defined __i386__)) || defined(C_ONLY) // rcg010212 - for PPC
 
 void Com_Memcpy (void* dest, const void* src, const size_t count)
 {
